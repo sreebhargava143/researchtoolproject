a07fee8fd53c69f983c6711d85afed465deb2db6
sreebhargava143 <sreebhargava143@gmail.com>
2019-10-15 18:24:05 +0530

    add postgres congif

--- researchtool/settings.py
+++ researchtool/settings.py
@@ -81,6 +81,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.environ.get("DB_NAME"),
+    #     'USER': os.environ.get("DB_USER"),
+    #     'PASSWORD': os.environ.get("DB_PASSWORD"),
+    #     'HOST': os.environ.get("DB_HOST"),
+    #     'PORT': os.environ.get("DB_PORT"),
+    # }
 }
 
 
