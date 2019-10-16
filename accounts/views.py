c8ec9ac055ab7e382cadfc3abcc9ff2c418f7c1e
uttaran wary <uttaran.wary@mountblue.io>
2019-10-16 20:21:50 +0530

    added registration functionality minus the password validation

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from accounts.forms import SignUpForm
+
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        print('post request')
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            print('form is valid')
+            if form.cleaned_data.get('password') == form.cleaned_data.get('password2'):
+                username = form.cleaned_data.get('username')
+                email = form.cleaned_data.get('email')
+                password = form.cleaned_data.get('password')
+                user = User(username=username, email=email, password=password)
+                user.save()
+    else:
+        form = SignUpForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
