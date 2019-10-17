bf350a48a50e7837f203b6cf62ad9cc63046f801
uttaran wary <uttaran.wary@mountblue.io>
2019-10-17 13:53:18 +0530

    fixed commit error

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,9 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.forms.widgets import PasswordInput
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+
+from crispy_forms.helper import FormHelper, Layout
+from crispy_forms.layout import Submit, Field, Fieldset, ButtonHolder
 
 
 class SignUpForm(forms.Form):
@@ -12,3 +14,16 @@ class SignUpForm(forms.Form):
     password = forms.CharField(label='Password',
         min_length=8, max_length=30, widget=PasswordInput, required=True)
     password2 = forms.CharField(label='Repeat Password', widget=PasswordInput, required=True)
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'register'
+        self.helper.layout = Layout(
+            Field('username', css_class='form-control',),
+            Field('email', css_class="form-control"),
+            Field('password', css_class="form-control"),
+            Field('password2', css_class="form-control"),
+            Submit('submit', 'Register')
+        )
