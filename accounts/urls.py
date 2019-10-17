8870123c468fc0afafd53b2c90f21f115dfb1c4c
mohankumar.sajja <mohan.sajja@mountblue.io>
2019-10-17 12:48:31 +0530

    Add main app and clear file structure

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     # post views
     # path('login/', views.user_login, name='login'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logged_out.html'), name='logout'),
     path('register/', views.register, name='register'),
 ]
