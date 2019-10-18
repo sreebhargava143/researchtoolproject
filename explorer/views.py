1cd91abfbfeb3da6ce5ba4ffb753f5ee5f65a63c
uttaran wary <uttaran.wary@mountblue.io>
2019-10-18 18:25:31 +0530

    search

--- explorer/views.py
+++ explorer/views.py
@@ -7,27 +7,27 @@ reddit = praw.Reddit(client_id='aN2WWXxjRA3FPQ',
                      user_agent='mac:prostalk:v0.0.1 (by nullbaka)')
 
 
-# def search_reddit(request):
-#     search_term = 'all'
-#     if request.method=="POST":
-#         form = SearchForm(request.POST)
-#         if form.is_valid():
-#             search_term = form.cleaned_data.get('search_term')
-#     subreddit = reddit.subreddit(search_term)
-#     submissions = subreddit.top()
-#     form = SearchForm()
-#     return render(request, 'explorer/reddit.html', {'submissions': submissions, 'form': form})
+def search_reddit(request):
+    search_term = 'all'
+    if request.method=="POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            search_term = form.cleaned_data.get('search_term')
+    subreddit = reddit.subreddit(search_term)
+    submissions = subreddit.top()
+    form = SearchForm()
+    return render(request, 'explorer/reddit.html', {'submissions': submissions, 'form': form})
 
 
 # def search_subreddit(request):
 
 
 
-# def comments(request, sub_name):
-#     submission = reddit.info([sub_name])
-#     for sub in submission:
-#         author = sub.author
-#         title = sub.title
-#         text = sub.selftext
-#         comments = sub.comments
-#     return render(request, 'explorer/comments.html', {'comments': comments, 'text': text, 'title': title, 'author': author})
+def comments(request, sub_name):
+    submission = reddit.info([sub_name])
+    for sub in submission:
+        author = sub.author
+        title = sub.title
+        text = sub.selftext
+        comments = sub.comments
+    return render(request, 'explorer/comments.html', {'comments': comments, 'text': text, 'title': title, 'author': author})
