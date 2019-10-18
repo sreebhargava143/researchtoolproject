6df4ae5e75d02907ac46495d16d97ce256c36cb4
uttaran wary <uttaran.wary@mountblue.io>
2019-10-18 10:26:05 +0530

    basic praw implementation

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logged_out.html'), name='logout'),
     path('register/', views.register, name='register'),
+    # path('search/', views.search_reddit, name='search'),
+    path('subreddit/', views.search_reddit, name='search'),
+    path('comments/', views.comments, name='comments')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
+import praw
 
 
 def register(request):
@@ -21,3 +22,17 @@ def register(request):
         form = SignUpForm()
 
     return render(request, 'accounts/register.html', {'form': form})
+
+reddit = praw.Reddit(client_id='aN2WWXxjRA3FPQ',
+                        client_secret='bEN3500-jFK0oLSkMoU52ywCeTI',
+                        user_agent='mac:prostalk:v0.0.1 (by nullbaka)')
+
+def search_reddit(request):
+    subreddit = reddit.subreddit('python')
+    hot = subreddit.hot(limit=5)
+    
+    return render(request, 'accounts/reddit.html', {'hot': hot})
+
+
+def comments(request, item):
+    return render(request, 'accounts/comments.html', {'item': item})
