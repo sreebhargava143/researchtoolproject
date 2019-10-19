8a74cea09872e28cced78e143dce348254ea8de3
mohankumar.sajja <mohan.sajja@mountblue.io>
2019-10-19 14:26:46 +0530

    Improve UI

--- explorer/views.py
+++ explorer/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 
+@login_required
 def explore(request):
     return render(request, 'explorer.html')
